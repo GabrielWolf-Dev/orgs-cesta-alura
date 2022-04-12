e32c7a67f70235a1d8bb0001d6303797e92c70dd
GabrielWolf-Dev <65361452+GabrielWolf-Dev@users.noreply.github.com>
2022-04-12 18:07:43 -0300

    Adicionando a lista de frutas nos mocks

--- src/mocks/basket.js
+++ src/mocks/basket.js
@@ -1,5 +1,11 @@
 // Assets:
 import logo from '../../assets/logo.png';
+import tomato from '../../assets/fruits/tomato.png';
+import broccoli from '../../assets/fruits/broccoli.png';
+import pumpkin from '../../assets/fruits/pumpkin.png';
+import cucumber from '../../assets/fruits/cucumber.png';
+import potato from '../../assets/fruits/potato.png';
+
 
 const basket = {
     top: {
@@ -11,6 +17,31 @@ const basket = {
         logo,
         description: "Uma cesta com produtos selecionados cuidadozamente da fazenda direto para sua cozinha",
         price: "R$ 40,00"
+    },
+    items: {
+        title: "Itens da cesta",
+        lista: [
+            {
+              nome: "Tomate",
+              imagem: tomato,
+            },
+            {
+              nome: "Brócolis",
+              imagem: broccoli,
+            },
+            {
+              nome: "Batata",
+              imagem: potato,
+            },
+            {
+              nome: "Pepino",
+              imagem: pumpkin,
+            },
+            {
+              nome: "Abóbora",
+              imagem: cucumber,
+            }
+        ]
     }
 };
 
