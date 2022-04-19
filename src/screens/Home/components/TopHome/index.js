7bb9c9d3cdb9f6361e47eae0565e08065622f0b6
GabrielWolf-Dev <65361452+GabrielWolf-Dev@users.noreply.github.com>
2022-04-19 18:54:38 -0300

    Começando a carrgar dinâmicamente os dados via os mocks com hooks

--- src/screens/Home/components/TopHome/index.js
+++ src/screens/Home/components/TopHome/index.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import { View, Text, Image } from 'react-native';
 
+import { loadHomeTop } from '../../../../services/loadDatas';
 /* Components & Styles */
 import styles from './style.js';
 
@@ -7,14 +9,20 @@ import styles from './style.js';
 import logo from '../../../../../assets/logo.png';
 
 export default function TopHome() {
+    const [dataTop, setDataTop] = useState({});
+
+    useEffect(() => {
+        setDataTop(loadHomeTop());
+    }, []);
+    
     return(
         <View style={styles.top}>
             <Image
                 style={styles.image}
                 source={logo}
             />
-            <Text style={styles.welcomeText}>Olá Gabriel</Text>
-            <Text style={styles.subtitle}>Encontre os melhores produtores</Text>
+            <Text style={styles.welcomeText}>{dataTop.welcome}</Text>
+            <Text style={styles.subtitle}>{dataTop.subtitle}</Text>
         </View>
     );
 }
